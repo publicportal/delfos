65229220ac323586e1b62572ead99d2fc55b3a80
publicportal <publicportal@github.com>
2024-05-08 13:32:48 +0000

    Networking and public instance

---
+++ oci/core.tf
@@ -0,0 +1,284 @@
+resource oci_core_vcn delfos {
+	cidr_blocks = [
+		"10.0.0.0/16",
+	]
+	compartment_id = var.compartment_ocid
+	display_name = "delfos"
+	dns_label = "delfos"
+	}
+
+resource oci_core_subnet public-subnet-delfos {
+	cidr_block = "10.0.0.0/24"
+	compartment_id = var.compartment_ocid 
+	display_name = "public subnet-delfos"
+	dns_label = "public"
+	prohibit_internet_ingress = "false"
+	prohibit_public_ip_on_vnic = "false"
+	vcn_id = oci_core_vcn.delfos.id
+}
+
+resource oci_core_subnet private-subnet-delfos {
+	cidr_block = "10.0.1.0/24"
+	compartment_id = var.compartment_ocid
+	display_name = "private subnet-delfos"
+	dns_label = "private"
+	prohibit_internet_ingress = "true"
+	prohibit_public_ip_on_vnic = "true"
+	route_table_id = oci_core_route_table.route-table-for-private-subnet-delfos.id
+	security_list_ids = [
+		oci_core_security_list.security-list-for-private-subnet-delfos.id,
+	]
+	vcn_id = oci_core_vcn.delfos.id
+}
+
+resource oci_core_default_route_table default-route-table-for-delfos {
+	compartment_id = var.compartment_ocid
+	display_name = "default route table for delfos"
+	manage_default_resource_id = oci_core_vcn.delfos.default_route_table_id
+	route_rules {
+		destination = "0.0.0.0/0"
+		destination_type = "CIDR_BLOCK"
+		network_entity_id = oci_core_internet_gateway.internet-gateway-delfos.id
+	}
+}
+
+
+
+resource oci_core_default_security_list default-security-list-for-delfos {
+  compartment_id = var.compartment_ocid
+  display_name = "Default Security List for delfos"
+  egress_security_rules {
+    destination      = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+    protocol  = "all"
+    stateless = "false"
+  }
+  ingress_security_rules {
+    protocol    = "6"
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+    stateless   = "false"
+    tcp_options {
+      max = "22"
+      min = "22"
+    }
+  }
+  ingress_security_rules {
+    icmp_options {
+      code = "4"
+      type = "3"
+    }
+    protocol    = "1"
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+    stateless   = "false"
+  }
+  ingress_security_rules {
+    icmp_options {
+      code = "-1"
+      type = "3"
+    }
+    protocol    = "1"
+    source      = "10.0.0.0/16"
+    source_type = "CIDR_BLOCK"
+    stateless   = "false"
+  }
+  manage_default_resource_id = oci_core_vcn.delfos.default_security_list_id
+}
+
+
+
+
+resource oci_core_route_table route-table-for-private-subnet-delfos {
+	compartment_id = var.compartment_ocid
+	display_name = "route table for private subnet-delfos"
+	route_rules {
+		destination = "0.0.0.0/0"
+		destination_type = "CIDR_BLOCK"
+		network_entity_id = oci_core_nat_gateway.nat-gateway-delfos.id
+	}
+	route_rules {
+		destination = "all-mad-services-in-oracle-services-network"
+		destination_type = "SERVICE_CIDR_BLOCK"
+		network_entity_id = oci_core_service_gateway.service-gateway-delfos.id
+	}
+	vcn_id = oci_core_vcn.delfos.id
+}
+
+resource oci_core_security_list security-list-for-private-subnet-delfos {
+	compartment_id = var.compartment_ocid
+	display_name = "security list for private subnet-delfos"
+	egress_security_rules {
+		destination = "0.0.0.0/0"
+		destination_type = "CIDR_BLOCK"
+		protocol = "all"
+		stateless = "false"
+	}
+	ingress_security_rules {
+		protocol = "6"
+		source = "10.0.0.0/16"
+		source_type = "CIDR_BLOCK"
+		stateless = "false"
+		tcp_options {
+			max = "22"
+			min = "22"
+		}
+	}
+	ingress_security_rules {
+		icmp_options {
+			code = "4"
+			type = "3"
+		}
+		protocol = "1"
+		source = "0.0.0.0/0"
+		source_type = "CIDR_BLOCK"
+		stateless = "false"
+	}
+	ingress_security_rules {
+		icmp_options {
+			code = "-1"
+			type = "3"
+		}
+		protocol = "1"
+		source = "10.0.0.0/16"
+		source_type = "CIDR_BLOCK"
+		stateless = "false"
+	}
+	vcn_id = oci_core_vcn.delfos.id
+}
+
+resource oci_core_nat_gateway nat-gateway-delfos {
+	block_traffic = "false"
+	compartment_id = var.compartment_ocid
+	display_name = "NAT gateway-delfos"
+	#TODO:review if ip is auto-assigned
+	#public_ip_id = "ocid1.publicip.oc1.eu-madrid-1.aaaaaaaasnmv7gj76u7n25hjij2jec56nebsiksv2kc3pjz6tt2lo42w2bpq"
+	vcn_id = oci_core_vcn.delfos.id
+}
+
+resource oci_core_internet_gateway internet-gateway-delfos {
+	compartment_id = var.compartment_ocid
+	display_name = "Internet gateway-delfos"
+	enabled = "true"
+	vcn_id = oci_core_vcn.delfos.id
+}
+
+data "oci_core_services" "all_oci_services" {
+	filter {
+		name = "name"
+		values = ["All .* Services In Oracle Services Network"]
+		regex = true
+	}
+	count = 1
+}
+
+resource oci_core_service_gateway service-gateway-delfos {
+	compartment_id = var.compartment_ocid
+	display_name = "Service gateway-delfos"
+	services {
+		service_id = lookup(data.oci_core_services.all_oci_services[0].services[0], "id")
+	}
+	vcn_id = oci_core_vcn.delfos.id
+}
+
+resource "oci_core_volume" "pitia-storage" {
+	availability_domain = "fkND:EU-MADRID-1-AD-1"
+	compartment_id = var.compartment_ocid
+	display_name = "pitia-storage"
+	is_auto_tune_enabled = "false"
+	size_in_gbs = "50"
+	vpus_per_gb = "10"
+}
+
+resource "oci_core_instance" "pitia" {
+	agent_config {
+		is_management_disabled = "false"
+		is_monitoring_disabled = "false"
+		plugins_config {
+			desired_state = "DISABLED"
+			name = "Vulnerability Scanning"
+		}
+		plugins_config {
+			desired_state = "DISABLED"
+			name = "Management Agent"
+		}
+		plugins_config {
+			desired_state = "ENABLED"
+			name = "Custom Logs Monitoring"
+		}
+		plugins_config {
+			desired_state = "DISABLED"
+			name = "Compute RDMA GPU Monitoring"
+		}
+		plugins_config {
+			desired_state = "ENABLED"
+			name = "Compute Instance Monitoring"
+		}
+		plugins_config {
+			desired_state = "DISABLED"
+			name = "Compute HPC RDMA Auto-Configuration"
+		}
+		plugins_config {
+			desired_state = "DISABLED"
+			name = "Compute HPC RDMA Authentication"
+		}
+		plugins_config {
+			desired_state = "ENABLED"
+			name = "Cloud Guard Workload Protection"
+		}
+		plugins_config {
+			desired_state = "DISABLED"
+			name = "Block Volume Management"
+		}
+		plugins_config {
+			desired_state = "DISABLED"
+			name = "Bastion"
+		}
+	}
+	availability_config {
+		is_live_migration_preferred = "false"
+		recovery_action = "RESTORE_INSTANCE"
+	}
+	availability_domain = "fkND:EU-MADRID-1-AD-1"
+	compartment_id = var.compartment_ocid
+	create_vnic_details {
+		assign_ipv6ip = "false"
+		assign_private_dns_record = "true"
+		assign_public_ip = "true"
+		subnet_id = oci_core_subnet.public-subnet-delfos.id
+	}
+	display_name = "pitia"
+	instance_options {
+		are_legacy_imds_endpoints_disabled = "false"
+	}
+	is_pv_encryption_in_transit_enabled = "true"
+	metadata = {
+		"ssh_authorized_keys" = var.ssh_authorized_keys
+	}
+	shape = "VM.Standard.A1.Flex"
+	shape_config {
+		memory_in_gbs = "12"
+		ocpus = "2"
+	}
+	source_details {
+		source_id = var.source_id
+		source_type = "image"
+	}
+}
+
+resource "oci_core_volume_attachment" "pitia_storage_attachment" {
+	#Required
+	attachment_type = "paravirtualized"
+	instance_id = oci_core_instance.pitia.id
+	volume_id = oci_core_volume.pitia-storage.id
+
+	#Optional
+	display_name = "pitia_storage"
+	is_pv_encryption_in_transit_enabled = "true"
+	is_read_only = "false"
+	is_shareable = "false"
+	depends_on = [
+		oci_core_volume.pitia-storage,
+		oci_core_instance.pitia
+	]
+}
---
+++ oci/vars.tf
@@ -0,0 +1,4 @@
+variable compartment_ocid { default = "ocid1.tenancy.oc1..aaaaaaaatoljj5vhijjdsxcuncvwm4bdede4hhutse37ln6zivtp5etrelza" }
+variable region { default = "eu-madrid-1" }
+variable source_id { default = "ocid1.image.oc1.eu-madrid-1.aaaaaaaaopqz7l22adkrh32xle75d367u5le4cbcalenqssee7kpzek2zera" }
+variable ssh_authorized_keys { type = string }
