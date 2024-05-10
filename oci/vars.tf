6497b26addc61a2dbee56e749aeeb5360a2bd121
publicportal <169136370+publicportal@users.noreply.github.com>
2024-05-10 09:26:58 +0200

    Ubuntu non-ARM source image

--- oci/vars.tf
+++ oci/vars.tf
@@ -1,4 +1,4 @@
 variable compartment_ocid { default = "ocid1.tenancy.oc1..aaaaaaaatoljj5vhijjdsxcuncvwm4bdede4hhutse37ln6zivtp5etrelza" }
 variable region { default = "eu-madrid-1" }
-variable source_id { default = "ocid1.image.oc1.eu-madrid-1.aaaaaaaaopqz7l22adkrh32xle75d367u5le4cbcalenqssee7kpzek2zera" }
+variable source_id { default = "ocid1.image.oc1.eu-madrid-1.aaaaaaaabtqgbapqdp4uarwgwlz6hjrcpmsijyjpju2vkftzvcqeuqcxdsga" }
 variable ssh_authorized_keys { type = string }
